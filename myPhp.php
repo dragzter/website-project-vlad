05c02cc10bfde6adf01e3ab596d979e35de35975
dragzter <vladimir.mujakovic@gmail.com>
2016-11-21 19:37:09 -0500

    Add files via upload

---
+++ myPhp.php
@@ -0,0 +1,27 @@
+
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $from = 'From: TangledDemo'; 
+    $to = 'vladimir.mujakovic@gmail.com'; 
+    $subject = 'Hello';
+    $human = $_POST['human'];
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+		
+    if ($_POST['submit'] && $human == '4') {				 
+        if (mail ($to, $subject, $body, $from, $phone)) { 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
+    }
+    
+?>
+
+  
+  
